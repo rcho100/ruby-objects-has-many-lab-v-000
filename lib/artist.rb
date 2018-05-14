ca8c94bfe0e582b4cd7a6691c5a9f7e11833c605
Richard Cho <richardcho100@gmail.com>
2018-05-14 18:23:44 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,7 +1,7 @@
 class Artist
   attr_accessor :name, :songs
 
-  @@song_count = []
+  @@song_count = 0
   def self.song_count
     @@song_count.size
   end
@@ -13,14 +13,14 @@ class Artist
 
   def add_song(song)
     @songs << song
-    @@song_count << song
+    @@song_count += @songs.count
     song.artist = self
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
-    @@song_count << song
+    @@song_count += @songs.count
     song.artist = self
     song
   end
