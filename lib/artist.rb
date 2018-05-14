a47e2f261a8a4a70c47d383884ab5ce0330debb5
Richard Cho <richardcho100@gmail.com>
2018-05-14 18:42:46 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -56,7 +56,7 @@ class Author
 
   def add_post_by_title(title)
     post = Post.new(title)
-    @@post_count += @posts
+    @@post_count += @posts.size
     @posts << post
     post.author = self
   end
