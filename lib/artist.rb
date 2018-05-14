319ce8a9e434d97003863fba39cc366da91fa309
Richard Cho <richardcho100@gmail.com>
2018-05-14 18:24:12 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -20,7 +20,7 @@ class Artist
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
-    @@song_count += @songs.count
+    #@@song_count += @songs.count
     song.artist = self
     song
   end
